908a8cc581bfd615e0df702a3744f2bf3d66ce22
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-01-24 15:57:41 +0530

    Create dot1.c

---
+++ dot1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char a[100];
+    int i,c=0;
+    scanf("%s",&a);
+    for(i=0;a[i]!='\0';i++)
+    c++;
+    for(i=0;i<=c;i++)
+    {
+    if(a[i]=='\0')
+    a[i]='.';
+    }
+    printf("%s",a);
+}
