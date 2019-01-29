3a191be3d3e582c09616ceee91a4ee30ceba0176
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-01-29 15:11:59 +0530

    Update 25.c

--- 25.c
+++ 25.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
- 
-main( )                                                     
-   
+void main( )                                                     
+  
 {                                                           
        
 int i,j,n;                                              
