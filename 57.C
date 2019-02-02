b85e0e2e556790102bfb280751d761b409fca054
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-02-02 08:58:05 -0800

    Create 57.C

---
+++ 57.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+   int n,k,i,a[10],c=0;
+   scanf("%d%d",&n,&k);
+   for(i=0;i<n;i++)
+   scanf("%d",&a[i]);
+   for(i=0;i<n;i++)
+   {
+       if(a[i]==k)
+       c++;
+   }
+   printf("%d",c);
+}
