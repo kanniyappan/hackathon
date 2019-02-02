bf51a3d21710b5f5f70a5404fdb4e528abd8a5e7
kanniyappan <36396182+kanniyappan@users.noreply.github.com>
2019-02-02 04:43:15 -0800

    Add files via upload

---
+++ 52.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main()
+{
+int n;
+scanf("%d",&n);
+if(n==1)
+printf("One");
+else if(n==2)
+printf("Two");
+else if(n==3)
+printf("Three");
+else if(n==4)
+printf("Four");
+else if(n==5)
+printf("Five");
+else if(n==6)
+printf("Six");
+else if(n==7)
+printf("Seven");
+else if(n==8)
+printf("Eight");
+else if(n==9)
+printf("Nine");
+else if(n==10)
+printf("Ten");
+else
+printf("Invalid Input");
+}
